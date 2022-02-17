2bfa5213f8a5f5de9e27802620b32da2cff4103f
Zahiriddin Adhamov <zahadhamov@gmail.com>
2022-02-18 02:12:00 +0300

    vrode gotovo

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import 'bootstrap';
 import './styles.scss';
 import view from './view.js';
 
-const form = document.querySelector('form');
-view(form);
+const app = () => {
+  const form = document.querySelector('form');
+  view(form);
+};
+
+app();
